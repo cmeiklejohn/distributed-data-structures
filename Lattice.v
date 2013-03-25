adb78158d67fabfc76d11c9351194b033e105588
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-03-25 01:52:09 -0700

    Update theorems to use specifics.

--- Lattice.v
+++ Lattice.v
@@ -19,11 +19,13 @@ Definition lbool_merge lb1 lb2 :=
     (LBool b1, LBool b2) => (LBool (orb b1 b2))
   end.
 
-Theorem lbool_merge_assoc : forall (b1 b2 b3 : bool),
-  lbool_merge (lbool_merge (LBool b1) (LBool b2)) (LBool b3) = 
-    lbool_merge (LBool b3) (lbool_merge (LBool b1) (LBool b2)).
+Theorem lbool_merge_assoc : forall (lb1 lb2 lb3 : lbool),
+  lbool_merge (lbool_merge lb1 lb2) lb3 = 
+    lbool_merge lb3 (lbool_merge lb1 lb2).
 Proof with eauto.
-  induction b1; induction b2; induction b3...
+  induction lb1; induction lb2; induction lb3...
+  unfold lbool_merge; unfold orb.  
+  destruct b; simpl; destruct b1; try destruct b0; try reflexivity.
 Qed.
 
 Theorem lbool_merge_comm : forall (b1 b2 : bool),
@@ -44,18 +46,18 @@ Definition lmax_merge lm1 lm2 :=
     (LMax n1, LMax n2) => (LMax (max n1 n2))
   end.
 
-Theorem lmax_merge_assoc : forall (n1 n2 n3 : nat),
-  lmax_merge (lmax_merge (LMax n1) (LMax n2)) (LMax n3) =
-    lmax_merge (LMax n3) (lmax_merge (LMax n1) (LMax n2)).
+Theorem lmax_merge_assoc : forall (lm1 lm2 lm3 : lmax),
+  lmax_merge (lmax_merge lm1 lm2) lm3 =
+    lmax_merge lm3 (lmax_merge lm1 lm2).
 Proof with eauto.
-  induction n1; induction n2; induction n3; try unfold lmax_merge; try eauto;
+  induction lm1; induction lm2; induction lm3; try unfold lmax_merge; try eauto;
   rewrite max_comm; reflexivity.
 Qed.
 
-Theorem lmax_merge_comm : forall (n1 n2 : nat),
-  lmax_merge (LMax n1) (LMax n2) = lmax_merge (LMax n2) (LMax n1).
+Theorem lmax_merge_comm : forall (lm1 lm2 : lmax),
+  lmax_merge lm1 lm2 = lmax_merge lm2 lm1.
 Proof with eauto.
-  induction n1; induction n2...
+  induction lm1; induction lm2....
   try unfold lmax_merge; rewrite max_comm...
 Qed.
 
@@ -71,18 +73,18 @@ Definition lmin_merge lm1 lm2 :=
     (LMin n1, LMin n2) => (LMin (min n1 n2))
   end.
 
-Theorem lmin_merge_assoc : forall (n1 n2 n3 : nat),
-  lmin_merge (lmin_merge (LMin n1) (LMin n2)) (LMin n3) =
-    lmin_merge (LMin n3) (lmin_merge (LMin n1) (LMin n2)).
+Theorem lmin_merge_assoc : forall (lm1 lm2 lm3 : lmin),
+  lmin_merge (lmin_merge lm1 lm2) lm3 =
+    lmin_merge lm3 (lmin_merge lm1 lm2).
 Proof with eauto.
-  induction n1; induction n2; induction n3; try unfold lmin_merge; try eauto;
+  induction lm1; induction lm2; induction lm3; try unfold lmin_merge; try eauto;
   rewrite min_comm; reflexivity.
 Qed.
 
-Theorem lmin_merge_comm : forall (n1 n2 : nat),
-  lmin_merge (LMin n1) (LMin n2) = lmin_merge (LMin n2) (LMin n1).
+Theorem lmin_merge_comm : forall (lm1 lm2 : lmin),
+  lmin_merge lm1 lm2 = lmin_merge lm2 lm1.
 Proof with eauto.
-  induction n1; induction n2...
+  induction lm1; induction lm2...
   try unfold lmin_merge; rewrite min_comm...
 Qed.
 
