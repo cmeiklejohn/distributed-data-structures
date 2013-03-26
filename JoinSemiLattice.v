a4515e4b0946956c2d46b6eeaa23c44b1ee80391
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-03-25 22:19:25 -0700

    Checkpoint polymorphic lmax.

--- JoinSemiLattice.v
+++ JoinSemiLattice.v
@@ -3,8 +3,6 @@ Require Export Max.
 Require Export Min.
 Require Export Sets.Ensembles.
 
-Set Implicit Arguments.
-
 Module JoinSemiLattice.
 
 Inductive lbool : Type :=
@@ -54,46 +52,46 @@ Proof with eauto.
   induction lb... destruct b...
 Qed.
 
-Inductive lmax : Type :=
-  | LMaxBottom : lmax
-  | LMaxValue : forall (n : nat), lmax.
+Inductive lmax (X : Type) : Type :=
+  | LMaxBottom : lmax X
+  | LMaxValue : X -> lmax X.
 
-Hint Constructors lmax.
+(* Definition lmax_reveal (X : Type) (lm : lmax X) : X := *)
+(*   match lm with *)
+(*   | LMaxBottom => match X with *)
+(*                     | nat => 0 *)
+(*                   end *)
+(*   | LMaxValue n => n *)
+(*   end. *)
 
-Definition lmax_reveal lm :=
-  match lm with
-    | LMaxBottom => 0
-    | LMaxValue n => n
-  end.
+(* Definition lmax_merge (X : Type) (lm1 lm2 : lmax X) := *)
+(*   match (lm1, lm2) with *)
+(*     | (LMaxBottom, LMaxBottom) => LMaxBottom *)
+(*     | (LMaxBottom, LMaxValue n) => LMaxBottom *)
+(*     | (LMaxValue n, LMaxBottom) => LMaxBottom *)
+(*     | (LMaxValue n1, LMaxValue n2) => LMaxValue X (max n1 n2) *)
+(*   end. *)
 
-Definition lmax_merge lm1 lm2 :=
-  match (lm1, lm2) with
-    | (LMaxBottom, LMaxBottom) => LMaxBottom
-    | (LMaxBottom, LMaxValue n) => LMaxValue n
-    | (LMaxValue n, LMaxBottom) => LMaxValue n
-    | (LMaxValue n1, LMaxValue n2) => (LMaxValue (max n1 n2))
-  end.
+(* Theorem lmax_merge_assoc : forall (lm1 lm2 lm3 : lmax), *)
+(*   lmax_merge (lmax_merge lm1 lm2) lm3 = *)
+(*     lmax_merge lm3 (lmax_merge lm1 lm2). *)
+(* Proof with eauto. *)
+(*   destruct lm1; destruct lm2; destruct lm3; eauto; *)
+(*     unfold lmax_merge; rewrite max_comm... *)
+(* Qed. *)
 
-Theorem lmax_merge_assoc : forall (lm1 lm2 lm3 : lmax),
-  lmax_merge (lmax_merge lm1 lm2) lm3 =
-    lmax_merge lm3 (lmax_merge lm1 lm2).
-Proof with eauto.
-  destruct lm1; destruct lm2; destruct lm3; eauto;
-    unfold lmax_merge; rewrite max_comm...
-Qed.
+(* Theorem lmax_merge_comm : forall (lm1 lm2 : lmax), *)
+(*   lmax_merge lm1 lm2 = lmax_merge lm2 lm1. *)
+(* Proof with eauto. *)
+(*   destruct lm1; destruct lm2... *)
+(*     try unfold lmax_merge; rewrite max_comm... *)
+(* Qed. *)
 
-Theorem lmax_merge_comm : forall (lm1 lm2 : lmax),
-  lmax_merge lm1 lm2 = lmax_merge lm2 lm1.
-Proof with eauto.
-  destruct lm1; destruct lm2...
-    try unfold lmax_merge; rewrite max_comm...
-Qed.
-
-Theorem lmax_merge_idemp : forall (lm : lmax),
-  lmax_merge lm lm = lm.
-Proof with eauto.
-  destruct lm; unfold lmax_merge...
-    rewrite max_idempotent...
-Qed.
+(* Theorem lmax_merge_idemp : forall (lm : lmax), *)
+(*   lmax_merge lm lm = lm. *)
+(* Proof with eauto. *)
+(*   destruct lm; unfold lmax_merge... *)
+(*     rewrite max_idempotent... *)
+(* Qed. *)
 
 End JoinSemiLattice.
