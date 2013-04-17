8a24e58e7cb4fcb7047a094862a4bfc7b8c7441c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-04-17 11:46:39 -0400

    Start sketching out a working G-Counter.

--- Counters.v
+++ Counters.v
@@ -1,37 +1,55 @@
-Require Export SfLib.
-Require Export Sets.Ensembles.
+Require Import Coq.FSets.FMaps.
+Require Import Coq.FSets.FSets.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Structures.OrdersEx.
+Require Import Coq.MSets.MSetList.
 
-(** Actor, which is of type nat. *)
-Definition Actor := nat.
+Module UOT_to_OrderedTypeLegacy (UOT:OrderedType) <:
+  (IsEqOrig UOT) <: OrderedType.OrderedType.
 
-(** Clock, representing an actor and count. *)
-Inductive Clock : Type :=
-  In_Clock : Actor -> nat -> Clock.
+  Definition t := UOT.t.
+  Definition lt := UOT.lt.
+  Definition eq := UOT.eq.
+  Definition eq_refl := let (r, _, _) := UOT.eq_equiv in r.
+  Definition eq_sym := let (_, s, _) := UOT.eq_equiv in s.
+  Definition eq_trans := let (_, _, t) := UOT.eq_equiv in t.
 
-Hint Constructors Clock.
+  Lemma lt_trans : forall x y z : t, UOT.lt x y -> UOT.lt y z -> UOT.lt x z.
+  Proof. destruct UOT.lt_strorder as [i t]. apply t. Qed.
 
-(** Grow only counter, representing a vector of clocks. *)
-Inductive G_Counter : Type :=
-  In_G_Counter : forall (x : Type), Ensemble x -> G_Counter.
+  Lemma lt_not_eq : forall x y : t, UOT.lt x y -> ~ UOT.eq x y.
+  Proof. destruct UOT.lt_strorder as [i t]. intros. intro. rewrite H0 in *.
+         exact (i y H). Qed.
 
-Hint Constructors G_Counter.
+  Definition compare (x y : t) : OrderedType.Compare UOT.lt UOT.eq x y :=
+    match (CompareSpec2Type (UOT.compare_spec x y)) with
+      | CompLtT l => OrderedType.LT l
+      | CompEqT e => OrderedType.EQ e
+      | CompGtT g => OrderedType.GT g
+    end.
 
-(** Add increment, merge and reveal functions for G_Counter. *)
+  Definition eq_dec := UOT.eq_dec.
+  Definition eq_equiv := UOT.eq_equiv.
+  Definition lt_strorder := UOT.lt_strorder.
+  Definition lt_compat := UOT.lt_compat.
+End UOT_to_OrderedTypeLegacy.
 
-(** Positive/negative counters, a composition of two G_Counters. *)
-Inductive PN_Counter : Type :=
-  In_PN_Counter : forall (x : Type), G_Counter -> G_Counter -> PN_Counter.
+Module Nat_as_Legacy_OT := UOT_to_OrderedTypeLegacy (Nat_as_OT).
 
-(** Tests for constructor code. *)
-Eval compute in Singleton Clock (In_Clock 1 1).
+Module ClockMap := FMapList.Make (Nat_as_Legacy_OT).
 
-Eval compute in In_Clock 1 1.
+(* Grow only counter, representing a vector of clocks which are nats. *)
 
-Eval compute in In_G_Counter Clock (Singleton Clock (In_Clock 1 1)).
+Definition G_Counter_init := ClockMap.empty nat.
 
-Eval compute in Singleton Clock (In_Clock 1 1).
+Eval compute in ClockMap.find 1 (ClockMap.empty nat).
 
-Eval compute in In_PN_Counter Clock 
-                              (In_G_Counter Clock (Singleton Clock (In_Clock 1 1)))                                      (In_G_Counter Clock (Singleton Clock (In_Clock 2 1))).
+Function G_Counter_incr actor clocks :=
+  match ClockMap.find actor clocks with
+    | None => ClockMap.add actor 1 clocks
+    | Some count => (ClockMap.add actor (S count) (ClockMap.remove actor clocks))
+  end.
 
-(** Add increment, decrement, merge and reveal functions for PN_Counter. *)
+Eval compute in ClockMap.find 1 (G_Counter_incr 1 (G_Counter_init)).
+
+Eval compute in ClockMap.find 2 (G_Counter_incr 1 (G_Counter_init)).
